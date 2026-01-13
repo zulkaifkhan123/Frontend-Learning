4a63e327453f07bed90b53f8adf5640de2c7f250
Zulkaif Khan <zulkaifkhan183@gmail.com>
2026-01-13 23:47:47 +0500

    add new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import UseStates from "./components/useStates.jsx"
+
+import { useState } from 'react'
 
 function App() {
+  let [state , setState] = useState(0);
+
+  let increase = () => {
+    console.log(state);
+    setState(state => state+1);
+    setState(state => state+1);
+    setState(state => state+1);
+    console.log(state);
+  }
+
+  let decrease = () => {
+    console.log(state);
+    setState(state-1);
+    console.log(state);
+  }
+
+
   return (
     <>
-    <div>App</div>
-    <UseStates />
+    <h1 className='text-5xl m-5'>{state}</h1>
+    <button className='border cursor-pointer border-amber-950 block m-4 p-4 rounded-2xl' onClick={increase}>increase</button>
+    <button className='border cursor-pointer border-amber-950 block m-4 p-4 rounded-2xl' onClick={decrease}>decrease</button>
     </>
   )
 }
 
 export default App
+
--- src/components/useStates.jsx
+++ src/components/useStates.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import "../index.css"
+
 
 function UseStates() {
-    let [state,setState] = useState(0)
-    let increase = () => setState(state + 1);
+  const users = [
+    { id: 1, name: "Ali", age: 22 },
+    { id: 2, name: "Ahmed", age: 25 },
+    { id: 3, name: "Sara", age: 21 }
+  ];
+  let [state,setState] = useState(users)
+  console.log(state);
+
   return (
     <>
-    <h1>{state}</h1>
-    <button onClick={increase}>increase</button>
-    <br />
-    <br />
-    <p>The value increases in the console because the function is executed and the variable is updated in memory, but the User Interface is not updated because React does not re-render the component when a regular variable changes.</p>
+    <p>hello world</p>
+    {
+      state.map((sta)=>{
+        return (
+        <div id='card' className='bg-red-500' key={sta.id}>
+          <h1>{sta.name}</h1>
+          <h2>{sta.age}</h2>
+        </div>
+        )
+      })
+    }
+
+    <p>WHY WE NOT USE REGULAR VARIABLES FOR CHANGES IN UI : The value increases in the console because the function is executed and the variable is updated in memory, but the User Interface is not updated because React does not re-render the component when a regular variable changes. it uses State variables for making changes in UI and storing data .</p>
     </>
   )
 }
